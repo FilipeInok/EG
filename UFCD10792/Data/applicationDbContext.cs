cf52f88b81b0c03e639fd71626dc6136b9c80556
Filipe Inok <filipe.inok@gmail.com>
2023-04-19 13:46:25 +0100

    Ligação ao SQL Server

---
+++ UFCD10792/Data/applicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace UFCD10792.Data
+{
+    public class applicationDbContext : DbContext
+    {
+
+    }
+}
---
+++ UFCD10792/Models/UFCD.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UFCD10792.Models
+{
+    public class UFCD
+    {
+        [Key]
+        public int id { get; set; }
+        [Required]
+        public string code { get; set; }
+        public string name { get; set; }
+        public int hours { get; set; }
+        public DateTime beguiningDate { get; set; }
+    }
+
+
+}
