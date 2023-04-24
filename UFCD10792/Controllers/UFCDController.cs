e237587c74371e8e2db802e25049b55241307a5f
Filipe Inok <filipe.inok@gmail.com>
2023-04-24 14:01:20 +0100

    aula de 24-04-2023

---
+++ UFCD10792/Controllers/UFCDController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UFCD10792.Controllers
+{
+    public class UFCDController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
