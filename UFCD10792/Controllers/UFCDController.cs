9a355eb78ec3c45bd3e7a5f154efca94f2b91e62
Filipe Inok <filipe.inok@gmail.com>
2023-04-27 11:11:54 +0100

    aula de 26/04/2023

--- UFCD10792/Controllers/UFCDController.cs
+++ UFCD10792/Controllers/UFCDController.cs
@@ -1,12 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using UFCD10792.Data;
+using UFCD10792.Models;
 
 namespace UFCD10792.Controllers
 {
     public class UFCDController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public UFCDController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<UFCD> objUFCDlist = _db.UFCD;
+            return View(objUFCDlist);
         }
+
     }
 }
