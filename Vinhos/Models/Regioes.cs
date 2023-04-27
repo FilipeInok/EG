7d55a0af4663e90da9ba3157f77c05a445a5c1b3
Filipe Inok <filipe.inok@gmail.com>
2023-04-27 14:03:48 +0100

    Aula de 27-04-2023

---
+++ Vinhos/Models/Regioes.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vinhos.Models
+{
+    public class Regioes
+    {
+        [Key]
+        public int id { get; set; }
+        [Required]
+        public string name { get; set; }
+        public string? description { get; set; }
+
+    }
+}
