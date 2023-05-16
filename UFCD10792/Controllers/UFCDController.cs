d2aa72d871ca330bacb6823813aea6b44dad93bf
Filipe Inok <filipe.inok@gmail.com>
2023-05-16 13:55:36 +0100

    aula de 16/05/2023

--- UFCD10792/Controllers/UFCDController.cs
+++ UFCD10792/Controllers/UFCDController.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.ModelBinding;
 using UFCD10792.Data;
 using UFCD10792.Models;
 
@@ -30,13 +29,43 @@ namespace UFCD10792.Controllers
         {
             if (objUFCD.name == objUFCD.code)
             {
-                ModelState.AddModelError("code", "Name não pode ser igual ao Code");
+                ModelState.AddModelError("name", "Name não pode ser igual ao Code");
             }
             if (ModelState.IsValid)
             {
                 _db.UFCD.Add(objUFCD);
                 _db.SaveChanges();
                 return RedirectToAction("Index"); 
+            }
+            return View(objUFCD);
+        }
+        //get
+        public IActionResult Edit(int? id)
+        {
+            if (id<=0 || id == null)
+            {
+                return NotFound();
+            }
+            var UFCDFromDB = _db.UFCD.Find(id);
+            var UFCDFromDBSOrD = _db.UFCD.SingleOrDefault(x => x.id == id);
+            var UFCDFromDBSingle = _db.UFCD.Single(x => x.id == id);
+            var UFCDFromDBFirstOrD = _db.UFCD.FirstOrDefault(x => x.id == id);
+            return View(UFCDFromDB);
+        }
+        //set(post)
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(UFCD objUFCD)
+        {
+            if (objUFCD.name == objUFCD.code)
+            {
+                ModelState.AddModelError("name", "Name não pode ser igual ao Code");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.UFCD.Add(objUFCD);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
             }
             return View(objUFCD);
         }
--- UFCD10792/Models/UFCD.cs
+++ UFCD10792/Models/UFCD.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace UFCD10792.Models
 {
@@ -7,10 +8,18 @@ namespace UFCD10792.Models
         [Key]
         public int id { get; set; }
         [Required]
+        [DisplayName("Código")]
         public string? code { get; set; }
+        [DisplayName("Nome da Unidade")]
         public string? name { get; set; }
-        public int hours { get; set; }
-        public DateTime beginingDate { get; set; } = DateTime.Now;
+        [DisplayName("Carga Horária")]
+        [Range(25,50,ErrorMessage ="Valores válidos entre 25 e 50")]
+
+        public int hours { get; set; }       
+        [DisplayName("Data de Inicio")]
+        public DateTime beginingDate { get; set; } = DateTime.UtcNow;
+       
+
     }
 
 
